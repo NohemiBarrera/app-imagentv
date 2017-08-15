ab88d75e02d070b28443003f83128c237f682ff0
YessiGarcia <aria.801900@gmail.com>
2017-08-15 12:04:06 -0500

    carpetas,dependencias y archivos

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+
+gulp.task('pug', function(){
+    gulp.src('./src/*.pug')
+    .pipe(pug({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./public'))
+});
+
+gulp.task('sass', function() {
+  gulp.src('./src/*.sass')
+    .pipe(sass({
+     	pretty:true
+     }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./src/*.pug', ['pug'])
+    gulp.watch('./src/*.sass', ['sass'])
+});
+
+
+gulp.task('default', ['sass', 'pug', 'watch']);
